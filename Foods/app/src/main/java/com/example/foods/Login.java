131717eb091261308a7f3aa88366cdebfeb22b58
lobo98 <kamilo4023@gmail.com>
2019-09-01 16:11:54 -0500

    Ejemplo de clase para el Login

--- Foods/app/src/main/java/com/example/foods/Login.java
+++ Foods/app/src/main/java/com/example/foods/Login.java
@@ -3,6 +3,8 @@ package com.example.foods;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void SignIn(View view) {
+        int a = 20;
+        int b = 20;
+
+        if (a == b) {
+            Toast.makeText(this, "Login Correcto", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Login Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
